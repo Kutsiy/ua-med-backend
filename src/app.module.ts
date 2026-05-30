90db7062fe028f4100408938a8e6e5c5f77057af
Egor <kycuj.egor2020@gmail.com>
2026-05-30 15:51:09 +0300

    feat: added graphql and libs/utils

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule, nativeLoggerOptions } from 'nestjs-pino';
 import { Response, Request } from 'express';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -44,6 +48,15 @@ import { Response, Request } from 'express';
             : undefined,
       },
       ...nativeLoggerOptions,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
     }),
   ],
   controllers: [AppController],
